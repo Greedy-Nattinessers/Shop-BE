8a5d4b18a1a50ab89b54f55118232fe11cee4e85
CenFongDusty <3231568490@qq.com>
2024-11-26 22:43:53 +0800

    feat: 购物车数据库orm映射和购物车商品basemodel

---
+++ Models/cart.py
@@ -0,0 +1,4 @@
+from pydantic import BaseModel
+#*yeah short but ... 
+class cartProduct(BaseModel):
+    id: str
--- Models/database.py
+++ Models/database.py
@@ -13,3 +13,9 @@ class UserDb(Base):
     username: Mapped[str] = mapped_column(Text, nullable=False, unique=True)
     email: Mapped[str] = mapped_column(Text, nullable=False, unique=True)
     password: Mapped[str] = mapped_column(Text, nullable=False)
+
+class UserCartDb(Base):
+    __tablename__ = "UserCart"
+    userUid:Mapped[str] = mapped_column(Text, primary_key=True)
+    productId: Mapped[str] = mapped_column(Text,primary_key=True)
+
